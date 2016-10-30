80980f39055b0f473766fc6ed34617ce2d218a52
Aneesha Bakharia <aneesha.bakharia@gmail.com>
2016-10-31 08:28:40 +1000

    courses to exclude

--- services/person_course/service.py
+++ services/person_course/service.py
@@ -96,7 +96,7 @@ class PersonCourse(base_service.BaseService):
             print self.courses.items()
             for course_id, course in self.courses.items():
                 print "running", course_id
-                if (course_id not in ['employ_101x_1T2016', 'hypers_301x_1T2015', 'ielts_x_3T2015', 'meta_101x_1T2016', 'think_101x_1T2014, 'think_101x_1T2016', 'tropic_101x_1T2014']): 
+                if (course_id not in ['employ_101x_1T2016', 'hypers_301x_1T2015', 'ielts_x_3T2015', 'meta_101x_1T2016', 'think_101x_1T2014', 'think_101x_1T2016', 'tropic_101x_1T2014']): 
                     # Get chapters from course info
                     json_file = course['dbname'].replace("_", "-") + '.json'
                     courseinfo = self.loadcourseinfo(json_file)
